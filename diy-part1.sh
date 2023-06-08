aa8d540f5a5c55c23f8c6d1ed4ed8d4fe489950a
Kool-Shine <127078964+Kool-Shine@users.noreply.github.com>
2023-06-08 16:09:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,7 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages.git' >>feeds.conf.default
+echo 'src-git luci_passwall https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
